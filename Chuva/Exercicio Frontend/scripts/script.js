7fb8063313369fe9ed740f20c91f7fec501b2b64
Márcio Nogueira <mar.nogueira07@gmail.com>
2022-05-18 10:46:44 -0300

    adcionado responsividade

--- Chuva/Exercicio Frontend/scripts/script.js
+++ Chuva/Exercicio Frontend/scripts/script.js
@@ -62,3 +62,21 @@ function send() {
     </div>
     `;
 }
+
+function dropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.droplink')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
